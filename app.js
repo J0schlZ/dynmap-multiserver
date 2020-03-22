c04e428bde93ad15796c9c999657ec5f3a0ab449
J0schlZ <j0schlz@gmx.net>
2020-03-22 01:22:44 +0100

    Read local files if possible, added fetching skins.

--- app.js
+++ app.js
@@ -13,10 +13,22 @@ const config = {
     crossOrigin     : false,
     configServer    : 'lobby',
     mapServer : {
-        'lobby'         : 'https://map.craft-together.de/lobby',
-        'freebuild1'    : 'https://map.craft-together.de/freebuild1',
-        'freebuild2'    : 'https://map.craft-together.de/freebuild2',
-        'dev'           : 'https://map.craft-together.de/dev'
+        'lobby' : {
+            url : 'https://map.craft-together.de/lobby',
+            files : '/var/www/vhosts/craft-together.de/subdomains/map/lobby'
+        },
+        'freebuild1' : {
+            url : 'https://map.craft-together.de/freebuild1',
+            files : '/var/www/vhosts/craft-together.de/subdomains/map/freebuild1'
+        },
+        'freebuild2' : {
+            url : 'https://map.craft-together.de/freebuild2',
+            files : '/var/www/vhosts/craft-together.de/subdomains/map/freebuild2'
+        },
+        'dev' : {
+            url : 'https://map.craft-together.de/dev',
+            files : '/var/www/vhosts/craft-together.de/subdomains/map/dev'
+        }
     }
 };
 
@@ -44,14 +56,14 @@ class DynmapMultiserver {
             }, this.config.checkInterval * 1000);
 
             this.app.listen(8123, function () {
-                console.log('MultiMap v' + version + ' started.');
+                console.log('dynmap-multiserver v' + version + ' started.');
             });
         });
     }
 
     async getConfiguration(configServer) {
         this.dynmap.configScript = await this.getConfigScript(configServer);
-        let serverConfig = this.getServerConfig();
+        this.getServerConfig();
     }
 
     tiles(req, res) {
@@ -59,7 +71,7 @@ class DynmapMultiserver {
             let match = /marker_([A-Z_0-9-]{0,}).json/gi.exec(req.params[0]);
 
             if (match) {
-                let worldName = match[1], serverName = null;
+                let worldName = match[1];
 
                 if (typeof (this.dynmap.markers[worldName]) === 'object')
                     res.json(this.dynmap.markers[worldName]);
@@ -92,8 +104,25 @@ class DynmapMultiserver {
         }
 
         else if (req.originalUrl.startsWith('/tiles/faces')) {
-            console.log(req.originalUrl);
-            res.sendStatus(404);
+            let match = /\/tiles\/faces\/([A-Z-_0-9]\w+)\/([A-Z-_0-9]{0,16}).([A-Z0-9]\w+)/gi.exec(req.originalUrl);
+
+            if (match) {
+                let size = match[1], playerName = match[2], location;
+
+                if (size === 'body')
+                    location = 'https://ctma.craft-together.de/api/skin/' + playerName + '/surgeplay/bust/32';
+                else
+                    location = 'https://ctma.craft-together.de/api/skin/' + playerName + '/surgeplay/head/16';
+
+                res.writeHead(302, {
+                    'Location': location
+                });
+                res.end();
+
+                return;
+            }
+
+            req.sendStatus(404);
         }
 
         else {
@@ -105,7 +134,7 @@ class DynmapMultiserver {
                 if (typeof (this.dynmap.worlds[worldName]) === 'object')
                     serverName = this.dynmap.worlds[worldName].server;
 
-                let location = this.config.mapServer[serverName] + '/' + req.path.substring(1);
+                let location = this.config.mapServer[serverName].url + '/' + req.path.substring(1);
 
                 res.writeHead(302, {
                     'Location': location
@@ -115,11 +144,9 @@ class DynmapMultiserver {
                 return;
             }
 
-            console.log(req.originalUrl);
+            console.log('Not handled: ' + req.originalUrl);
             res.sendStatus(404);
         }
-
-        return;
     }
 
     standalone(req, res) {
@@ -129,6 +156,7 @@ class DynmapMultiserver {
             if (match) {
                 let worldInfo = this.dynmap.maps[match[1]] || {};
                 worldInfo.players = this.dynmap.players;
+                worldInfo.currentcount = Object.keys(this.dynmap.players).length;
                 res.json(worldInfo);
                 return;
             }
@@ -142,7 +170,7 @@ class DynmapMultiserver {
 
             case "config.js":
                 if (this.dynmap.configScript === null || this.dynmap.configScript === {}
-                ||  this.dynmap.server[config.configServer] == null || this.dynmap.server[config.configServer] == {})
+                    ||  this.dynmap.server[config.configServer] == null || this.dynmap.server[config.configServer] === {})
                     res.end('alert(\'MultiMap v' + version + ' is not ready. Please try again later.\');');
                 else
                     res.end(this.dynmap.configScript);
@@ -174,20 +202,30 @@ class DynmapMultiserver {
     }
 
     async getJSONFile(url) {
-        let content = null,
-            response = await Request(url);
+        let content, response = null;
 
         try {
+            response = await Request(url);
             content = JSON.parse(response);
         } catch (err) {
-            return Promise.reject(err);
+            console.log(err.stack);
+            return {};
         }
 
         return content;
     }
 
-    async getConfigScript(configServer) {
-        let response = await Request(this.config.mapServer[configServer] + '/standalone/config.js');
+    async getConfigScript(serverName) {
+        let configScript = null;
+
+        try {
+            if (typeof (this.config.mapServer[serverName].files) === 'string' && FS.existsSync(this.config.mapServer[serverName].files))
+                configScript = (await FS.promises.readFile(this.config.mapServer[serverName].files + '/standalone/config.js')).toString();
+            else
+                configScript = await Request(this.config.mapServer[serverName].url + '/standalone/config.js');
+        } catch (err) {
+            console.log(err.stack);
+        }
 
         /*let config = {},
             lines  = response.replace(/\r\n/g, '\n').split('\n');
@@ -200,12 +238,25 @@ class DynmapMultiserver {
         //response = response.replace(/standalone\/dynmap_config.json/gi, '{server}/standalone/dynmap_config.json');
         //response = response.replace(/standalone\/dynmap_{world}.json/gi, '{server}/standalone/dynmap_{world}.json');
 
-        return response;
+        return configScript;
     }
 
     async getServerConfig(serverName = null) {
         if (serverName !== null) {
-            let config = await this.getJSONFile(this.config.mapServer[serverName] + '/standalone/dynmap_config.json');
+            let config;
+
+            try {
+                if (typeof (this.config.mapServer[serverName].files) === 'string' && FS.existsSync(this.config.mapServer[serverName].files)) {
+                    let cfgJson = await FS.promises.readFile(this.config.mapServer[serverName].files + '/standalone/dynmap_config.json');
+                    config = JSON.parse(cfgJson.toString());
+                } else
+                    config = await this.getJSONFile(this.config.mapServer[serverName].url + '/standalone/dynmap_config.json');
+            }
+            catch (err) {
+                console.log(err);
+                return null;
+            }
+
             config.servername = serverName;
 
             let server = {
@@ -218,13 +269,31 @@ class DynmapMultiserver {
 
             for (let i1 in config.worlds) {
                 let world = config.worlds[i1];
-                    world.server = serverName;
+                world.server = serverName;
 
                 this.dynmap.worlds[world.name] = world;
                 server.worlds[world.name] = world;
 
-                let map = await this.getJSONFile(this.config.mapServer[serverName] + '/standalone/dynmap_' + world.name + '.json'),
-                    markers = await this.getJSONFile(this.config.mapServer[serverName] + '/tiles/_markers_/marker_' + world.name + '.json');
+                let map, markers;
+
+                if (typeof (this.config.mapServer[serverName].files) === 'string' && FS.existsSync(this.config.mapServer[serverName].files)) {
+                    try {
+                        let mapJson = await FS.promises.readFile(this.config.mapServer[serverName].files + '/standalone/dynmap_' + world.name + '.json');
+                        map = JSON.parse(mapJson.toString());
+                        let markersJson = await FS.promises.readFile(this.config.mapServer[serverName].files + '/tiles/_markers_/marker_' + world.name + '.json');
+                        markers = JSON.parse(markersJson.toString());
+                    }
+                    catch (err) {
+                        console.log(err);
+                        return null;
+                    }
+                }
+                else {
+                    map = await this.getJSONFile(this.config.mapServer[serverName].url + '/standalone/dynmap_' + world.name + '.json');
+                    markers = await this.getJSONFile(this.config.mapServer[serverName].url + '/tiles/_markers_/marker_' + world.name + '.json');
+                }
+
+                map.server = serverName;
 
                 this.dynmap.maps[world.name] = map;
                 server.maps[world.name] = map;
@@ -234,7 +303,7 @@ class DynmapMultiserver {
 
                 for (let i2 in map.players) {
                     let player = map.players[i2];
-                        player.server = serverName;
+                    player.server = serverName;
 
                     this.dynmap.players[player.account] = player;
                     server.players[player.account] = player;
@@ -257,7 +326,7 @@ class DynmapMultiserver {
 
 if (!String.prototype.startsWith) {
     String.prototype.startsWith = function(searchString) {
-        return (haystack.lastIndexOf(searchString, 0) === 0) ? true : false;
+        return (haystack.lastIndexOf(searchString, 0) === 0);
     };
 }
 
